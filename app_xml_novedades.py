93763aef33dd9bad62e7e4dbcae28c564438d24c
Vladimir Alonso Barahona <details_quiconque.7j@icloud.com>
2025-06-05 19:10:55 -0500

    Update app_xml_novedades.py

--- app_xml_novedades.py
+++ app_xml_novedades.py
@@ -93,7 +93,7 @@ if xls_file:
         fecha_Desembolso = date(2025, 5, 9) # indicar fecha desembolso
         cod_programa = '126' # indicar código del programa
         cod_intermediario = '203018' # indicar código del intermediario
-        st.write(f"Tipo plan: {tipo_plan}")
+        #st.write(f"Tipo plan: {tipo_plan}")
 	tipo_plan = 0 # solo va 1 o cero | # si tipo_plan = 1 entonces bullet sino cuotas capital simétricas
         
         st.dataframe(df)
